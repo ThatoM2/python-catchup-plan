7101bcd2e13ebfad146044c29a55df03d60a9d49
thmogaljhb <thmogaljhb024@student.wethinkcode.co.za>
2025-05-12 14:55:28 +0200

    complete calculator exercise

--- homework/monday/calculator/calculator.py
+++ homework/monday/calculator/calculator.py
@@ -1,24 +1,36 @@
 """
 Task 1: Add 27 to 13 and print the results.
 """
+value_1 = 27
+value_2 = 13
+sum = value_1 + value_2
+print(sum)
 
 """
 Task 2: Subtract 13 from 97 and print the results.
 """
+print(97 - value_2)
 
 """
 Task 3: Divide 40 by 5 and print the results.
 """
-
+value_3 = 40
+value_4 = 5
+division = 40 / 5
+print(division)
 """
 Task 4: Create a variable named x and set its value to 3. Add 1 to x and print the results.
 """
+x = 3
+sum = x + 1
+print(sum)
 
-"""
+"""""
 Task 5: Fix the code below so that it works. This code should store "codewa.rs" as a variable called "name" but it's not working. Can you figure out why?
 """
 
-a == "code"
-b == "wa.rs"
-name == a + b
+a = "code"
+b = "wa.rs"
+name = a + b
+print(name)    #The problem was that double equal signs were used to assign the values. Only one assignment operator is needed.
 
