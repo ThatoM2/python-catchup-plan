93b87d66e17b65e1f3b77637811a0dca466c9544
Thato Mogale <thato0087@gmail.com>
2025-05-15 01:12:25 +0200

    add a function

--- homework/tuesday/functions/functions.py
+++ homework/tuesday/functions/functions.py
@@ -1,29 +1,39 @@
 # Calculate squares and cubes of different numbers
 
-number = 2
+'''number = 2
 square = number * number
 cube = number * number * number
 print("Number:", number)
 print("Square:", square)
-print("Cube:", cube)
+print("Cube:", cube)'''
 
-number = 4
-square = number * number
-cube = number * number * number
-print("Number:", number)
-print("Square:", square)
-print("Cube:", cube)
+def squares_and_cubes(num):
+    square = num * num
+    cube = num * num * num
+    print(f"Number: {num} ")
+    print(f"Square is: {square}")
+    print(f"Cube is: {cube}")
 
-number = 6
-square = number * number
-cube = number * number * number
-print("Number:", number)
-print("Square:", square)
-print("Cube:", cube)
+squares_and_cubes(6)
 
-number = 8
-square = number * number
-cube = number * number * number
-print("Number:", number)
-print("Square:", square)
-print("Cube:", cube)
+
+# number = 4
+# square = number * number
+# cube = number * number * number
+# print("Number:", number)
+# print("Square:", square)
+# print("Cube:", cube)
+
+# number = 6
+# square = number * number
+# cube = number * number * number
+# print("Number:", number)
+# print("Square:", square)
+# print("Cube:", cube)
+
+# number = 8
+# square = number * number
+# cube = number * number * number
+# print("Number:", number)
+# print("Square:", square)
+# print("Cube:", cube)
