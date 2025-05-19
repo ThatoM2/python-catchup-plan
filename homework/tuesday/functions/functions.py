1fa98018ff8991b6bd6e0b96584eefa9ede5e55f
Thato Mogale <thato0087@gmail.com>
2025-05-19 18:14:40 +0200

    modified function

--- homework/tuesday/functions/functions.py
+++ homework/tuesday/functions/functions.py
@@ -1,20 +1,11 @@
 # Calculate squares and cubes of different numbers
 
-'''number = 2
-square = number * number
-cube = number * number * number
-print("Number:", number)
-print("Square:", square)
-print("Cube:", cube)'''
-
-def squares_and_cubes(num):
-    square = num * num
-    cube = num * num * num
-    print(f"Number: {num} ")
-    print(f"Square is: {square}")
-    print(f"Cube is: {cube}")
-
-squares_and_cubes(6)
+# number = 2
+# square = number * number
+# cube = number * number * number
+# print("Number:", number)
+# print("Square:", square)
+# print("Cube:", cube)
 
 
 # number = 4
@@ -37,3 +28,16 @@ squares_and_cubes(6)
 # print("Number:", number)
 # print("Square:", square)
 # print("Cube:", cube)
+
+def squares(num):
+    print(f"square is: {num*num}")
+    return  num*num
+
+print(squares(2))
+
+def cubes(num):
+     print(f"Cube is: {num*num*num}")
+     return num*num*num
+
+print(cubes(2))
+ 
