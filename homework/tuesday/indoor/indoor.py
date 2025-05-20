b44fc4ee8334251af244f139af41400a048fe55e
Thato Mogale <thato0087@gmail.com>
2025-05-20 16:22:54 +0200

    add indoor

--- homework/tuesday/indoor/indoor.py
+++ homework/tuesday/indoor/indoor.py
@@ -1,10 +1,15 @@
 def main():
     # get input from user
+    user_input = input("Write something: ")
+    #return user_input
     # parse the input to indoor
+    output = indoor(user_input)
     # print the returned value of indoor
-    pass
+    print(output)
+    
 
 def indoor(str):
-    pass
+    user_input = str.lower()
+    return user_input 
 
 main()
